8a86e6860d2d6abad8601dcfedfe135245b79ba0
Florian Ianculescu <florian.ianculescu@gmail.com>
2025-10-02 22:13:41 +0300

    spring initializr

---
+++ src/main/java/ro/everyclass/EveryClass/EveryClassApplication.java
@@ -0,0 +1,13 @@
+package ro.everyclass.EveryClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/everyclass/EveryClass/EveryClassApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.everyclass.EveryClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
